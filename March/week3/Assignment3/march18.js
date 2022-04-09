66f1c8d15b32d33b11602b0f6b55d6970b8c874f
sayantan <sayantan.roy123789@gmail.com>
2022-04-09 21:08:58 +0530

    assignment 14 submmit

---
+++ March/week3/Assignment3/march18.js
@@ -0,0 +1,9 @@
+/**********************************************
+ * Question 1:program to write first 1o numbers.
+ ***********************************************/
+function first10Numbers(){
+    for(let i = 0;i < 10; i++){
+        console.log(i);
+    }
+}
+
