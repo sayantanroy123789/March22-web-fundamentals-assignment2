4a73e6b456e5c1affa020aea84aadc997c099203
sayantan <sayantan.roy123789@gmail.com>
2022-04-09 21:15:02 +0530

    assignment 14 submit

--- March/week3/Assignment3/march18.js
+++ March/week3/Assignment3/march18.js
@@ -7,3 +7,41 @@ function first10Numbers(){
     }
 }
 
+
+for(i=0;i<10;i++){
+    if(i==5)
+    {
+        break;
+    }
+    console.log(i);
+}
+
+
+
+int day;
+print("enter week day number 1 to 7");
+switch(day){
+    case 1:
+    print("1-sunday");
+    break;
+    case 2:
+     print("2-monday");
+     break;
+     case 3:
+     print("3-tuesday");
+     break;
+     case 4:
+    print("4-wednestday");
+    break; 
+    case 5:
+    print("5-thursday");
+    break; 
+    case 6:
+    print("6-friday");
+    break; 
+    case 7:
+    print("7-saturday");
+    break;  
+}                
+    
+
